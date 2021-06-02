5f1731639d1d234761461e7c3651205447b8dbd0
CHNUTT <chokwittaya.nutt@gmail.com>
2021-06-02 10:59:28 -0500

    [ Components - CarList / CarItem ] - implement CarList to render list of CarItem component using <FlatList> / Adjust CarItem to take correct props structure and fix CSS property

--- app/components/CarItem/style.js
+++ app/components/CarItem/style.js
@@ -1,7 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
-  carContainer: { width: '100%', height: '100%' },
+  carContainer: { width: '100%', height: Dimensions.get('window').height },
   titleWrapper: {
     marginTop: '30%',
     width: '100%',
