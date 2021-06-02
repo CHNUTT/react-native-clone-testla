e01b0ad542507163e232e84e535149d3bdf57c8d
CHNUTT <chokwittaya.nutt@gmail.com>
2021-06-02 09:29:40 -0500

    [ Components - CarItem ] - separate the CarItem into its own component

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import CarItem from './app/components/CarItem';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.carContainer}>
-        <ImageBackground
-          resizeMode='cover'
-          source={require('./app/assets/images/ModelX.jpeg')}
-          style={styles.image}
-        >
-          <View style={styles.titleWrapper}>
-            <Text style={styles.title}>Model S</Text>
-            <Text style={styles.subtitle}>Starting at $69,420</Text>
-          </View>
-        </ImageBackground>
-      </View>
+      <CarItem />
       <StatusBar style='auto' />
     </View>
   );
@@ -28,21 +18,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  carContainer: { width: '100%', height: '100%' },
-  titleWrapper: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center',
-  },
-  title: { fontSize: 40, fontWeight: '500' },
-  subtitle: {
-    fontSize: 16,
-    color: '#5c5e62',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
   },
 });
 
