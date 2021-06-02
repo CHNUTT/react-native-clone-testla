3ee14d3a45273edb716f8254a1d1df85adddbe5e
CHNUTT <chokwittaya.nutt@gmail.com>
2021-06-02 09:01:54 -0500

    [ Screen - Home ] - Render Title

--- App.js
+++ App.js
@@ -2,14 +2,19 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.carContainer}>
+        <View style={styles.titleWrapper}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at $69,420</Text>
+        </View>
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +23,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  carContainer: { width: '100%', height: '100%' },
+  titleWrapper: {
+    marginTop: '30%',
+    width: '100%',
+    alignItems: 'center',
+  },
+  title: { fontSize: 40, fontWeight: '500' },
+  subtitle: {
+    fontSize: 16,
+    color: '#5c5e62',
+  },
 });
+
+export default App;
