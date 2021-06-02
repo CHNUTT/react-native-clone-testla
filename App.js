54e58b434a722eb4bd1c0bd970776524eaa53e3e
CHNUTT <chokwittaya.nutt@gmail.com>
2021-06-02 09:09:08 -0500

    [ Screen - Home ] - Render BackgroundImage

--- App.js
+++ App.js
@@ -1,15 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 const App = () => {
   return (
     <View style={styles.container}>
       <View style={styles.carContainer}>
-        <View style={styles.titleWrapper}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subtitle}>Starting at $69,420</Text>
-        </View>
+        <ImageBackground
+          resizeMode='cover'
+          source={require('./app/assets/images/ModelX.jpeg')}
+          style={styles.image}
+        >
+          <View style={styles.titleWrapper}>
+            <Text style={styles.title}>Model S</Text>
+            <Text style={styles.subtitle}>Starting at $69,420</Text>
+          </View>
+        </ImageBackground>
       </View>
       <StatusBar style='auto' />
     </View>
@@ -33,6 +39,10 @@ const styles = StyleSheet.create({
   subtitle: {
     fontSize: 16,
     color: '#5c5e62',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
   },
 });
 
