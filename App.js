c6ce18f978f978d1cb5cdd6887790695449e609f
CHNUTT <chokwittaya.nutt@gmail.com>
2021-06-02 10:59:28 -0500

    [ Components - CarList / CarItem ] - implement CarList to render list of CarItem component using <FlatList> / Adjust CarItem to take correct props structure and fix CSS property

--- App.js
+++ App.js
@@ -1,17 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import CarItem from './app/components/CarItem';
+import CarList from './app/components/CarList';
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <CarItem
-        name='Modex X'
-        tagline='Starting from $99,500'
-        image={require('./app/assets/images/Model3.jpeg')}
-        taglineCTA='Touchless Delivery'
-      />
+      <CarList />
       <StatusBar style='auto' />
     </View>
   );
